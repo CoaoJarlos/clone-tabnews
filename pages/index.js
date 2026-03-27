40258f34706d2e37221ea8c27d33b1a109d878e3
JoaoCarlos <89268452+CoaoJarlos@users.noreply.github.com>
2026-03-27 21:31:01 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Acredite no impossível e trabalhe duro para que o impossível se torne inevitável, vai dar tudo certo 😎😎😎</h1>
+function Home() {
+  return (
+    <h1>
+      Acredite no impossível e trabalhe duro para que o impossível se torne
+      inevitável, vai dar tudo certo 😎😎😎
+    </h1>
+  );
 }
 
 export default Home;
